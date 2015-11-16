7fc7aad19c3762362b5fac8b12e5a93e51f1abf3
Jacob Erskov Halland <jaha@kvuc.dk>
2015-11-16 15:55:36 +0100

    autoCommit by terminal

--- parallax_soegning.js
+++ parallax_soegning.js
@@ -1,23 +1,19 @@
 $(document).ready(function() {
 // init controller
-	//var controller = new ScrollMagic.Controller();
-	//var controller = new ScrollMagic.Controller({container: '#scroll-wrapper'});
+	var controller = new ScrollMagic.Controller();
 
-
-///// inkluder iScroll for at få kontinuerlig scrollposition på alle devices
 
 	var isMobile = (function(a){return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
 	 console.log('isMobile ' + isMobile);
 
 	if(isMobile == true) {
 		var controller = new ScrollMagic.Controller({container: '#scroll-wrapper'});
-		// $('.scene').css('height', '80%');
+		$('#scroll-wrapper').css('overflow', 'scroll');
 	} else {
 		var controller = new ScrollMagic.Controller();
 	}
 
-
-
+// if(isMobile == true) {
 // 	var myScroll;
 // 	$(document).ready(function() {
 // 		// wrap for iScroll
@@ -70,7 +66,7 @@ $(document).ready(function() {
 	//Fade du skal lære metode ind
 	var timeLineIntro2 = new TimelineMax();
 	timeLineIntro2
-		.from('#intro-container-2', 1, {opacity : 0, scale: 0, delay:0.3});
+		.from('#intro-container-2', 1, {opacity : 0, scale: 0, delay:0.4});
 
 	var timeLineIntro2span = new TimelineMax();
 	timeLineIntro2span
@@ -352,7 +348,7 @@ $(document).ready(function() {
 		triggerElement: '#scene-2 .right .spacer',
 		triggerHook:'onLeave',
 	 	duration: '283%'
-	}).setPin('#quest-1');
+	}).setPin('#quest-1-pin');
 
 	var scene1Time3 = new ScrollMagic.Scene({
 		triggerElement: '#scene-3',
@@ -402,7 +398,7 @@ $(document).ready(function() {
 		triggerElement: '#scene-4 .right .spacer',
 		triggerHook:'onLeave',
 	 	duration: '330%'
-	}).setPin('#quest-2');
+	}).setPin('#quest-2-pin');
 
 	var hvemTime3 = new ScrollMagic.Scene({
 		triggerElement: '#scene-5',
@@ -417,7 +413,7 @@ $(document).ready(function() {
 	}).setPin('#expl-container-2');
 
 //scener tilføjes til controller
-	controller.addScene([
+controller.addScene([
 		hvem1, hvemTime, hvemTime3, hvemQuest, hvemExpl
 	]);
 
@@ -452,7 +448,7 @@ $(document).ready(function() {
 		triggerElement: '#hvornaar-1 .right .spacer',
 		triggerHook:'onLeave',
 	 	duration: '280%'
-	}).setPin('#quest-3');
+	}).setPin('#quest-3-pin');
 
 	var hvornaarTime3 = new ScrollMagic.Scene({
 		triggerElement: '#hvornaar-2',
@@ -467,7 +463,7 @@ $(document).ready(function() {
 	}).setPin('#expl-container-3');
 
 //scener tilføjes til controller
-	controller.addScene([
+controller.addScene([
 		hvornaar1, hvornaarTime, hvornaarTime3, hvornaarQuest, hvornaarExpl
 	]);
 
@@ -502,7 +498,7 @@ $(document).ready(function() {
 		triggerElement: '#til-hvem-1 .right .spacer',
 		triggerHook:'onLeave',
 	 	duration: '273%'
-	}).setPin('#quest-4');
+	}).setPin('#quest-4-pin');
 
 	var tilHvemTime3 = new ScrollMagic.Scene({
 		triggerElement: '#til-hvem-2',
@@ -520,7 +516,7 @@ $(document).ready(function() {
 	}).setPin('#expl-container-4');
 
 //scener tilføjes til controller
-	controller.addScene([
+controller.addScene([
 		tilHvem1, tilHvemTime, tilHvemTime3, tilHvemQuest, tilHvemExpl
 	]);
 
@@ -553,7 +549,7 @@ $(document).ready(function() {
 		triggerElement: '#om-hvad-1 .right .spacer',
 		triggerHook:'onLeave',
 	 	duration: '283%'
-	}).setPin('#quest-5');
+	}).setPin('#quest-5-pin');
 
 	var omHvadTime3 = new ScrollMagic.Scene({
 		triggerElement: '#om-hvad-2',
@@ -571,7 +567,7 @@ $(document).ready(function() {
 	}).setPin('#expl-container-5');
 
 //scener tilføjes til controller
-	controller.addScene([
+controller.addScene([
 		omHvad1, omHvadTime, omHvadTime3, omHvadQuest, omHvadExpl
 	]);
 
@@ -603,7 +599,7 @@ $(document).ready(function() {
 		triggerElement: '#virkemidler-1 .right .spacer',
 		triggerHook:'onLeave',
 	 	duration: '254%'
-	}).setPin('#quest-6');
+	}).setPin('#quest-6-pin');
 
 	var virkemidlerTime3 = new ScrollMagic.Scene({
 		triggerElement: '#virkemidler-2',
@@ -621,7 +617,7 @@ $(document).ready(function() {
 	}).setPin('#expl-container-6');
 
 //scener tilføjes til controller
-	controller.addScene([
+controller.addScene([
 		virkemidler1, virkemidlerTime, virkemidlerTime3, virkemidlerQuest, virkemidlerExpl
 	]);
 
@@ -654,7 +650,7 @@ $(document).ready(function() {
 		triggerElement: '#konsekvenser-1 .right .spacer',
 		triggerHook:'onLeave',
 	 	duration: '400%'
-	}).setPin('#quest-7');
+	}).setPin('#quest-7-pin');
 
 	var konsekvenserTime3 = new ScrollMagic.Scene({
 		triggerElement: '#konsekvenser-2',
@@ -672,7 +668,7 @@ $(document).ready(function() {
 	}).setPin('#expl-container-7');
 
 //scener tilføjes til controller
-	controller.addScene([
+controller.addScene([
 		konsekvenser1, konsekvenserTime, konsekvenserTime3, konsekvenserQuest, konsekvenserExpl
 	]);
 
